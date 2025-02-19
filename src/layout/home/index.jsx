83ebe97ec3bcad71e482618590b7609fe758cbf5
Dilshad <hmydilshadkt@gmail.com>
2025-02-19 11:48:30 +0530

    feat : protect routes

--- src/layout/home/index.jsx
+++ src/layout/home/index.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import AuthHeader from "../../components/header/auth";
 
 const HomeLayout = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (pathname === "/") {
+      navigate("/branches");
+    }
+  }, []);
   return (
     <section className="h-screen w-full flex flex-col  bg-[#141414]">
       <AuthHeader />
