4ca58e3e796cd3f20916a15fbbfcdf2efa8444ac
Dilshad <hmydilshadkt@gmail.com>
2025-02-19 11:35:02 +0530

    feat : bracnhes section

--- src/components/dashboard/selling/topSellingBranches/index.jsx
+++ src/components/dashboard/selling/topSellingBranches/index.jsx
@@ -11,27 +11,9 @@ const TopSellingBranches = ({ className }) => {
     setFullScreenGraph,
     setFullScreenModalOpen,
   } = UseCommon();
-  const { data: dummy } = useQuery("topSellingBranches", topSellingBranches);
-  const data = [
-    {
-      name: "Al Hamdan",
-      location: "Dubai",
-      value: 5400.0,
-      grow: true, // Indicates the value is increasing
-    },
-    {
-      name: "Al Khaleej",
-      location: "Sharjah",
-      value: 4658.0,
-      grow: false, // Indicates the value is decreasing
-    },
-    {
-      name: "Al Safa",
-      location: "Abu Dhabi",
-      value: 5658.0,
-      grow: true, // Indicates the value is increasing
-    },
-  ];
+  const { data: dummy } = useQuery("topSellingBranches", topSellingBranches, {
+    select: (data) => data?.data?.salesData,
+  });
 
   return (
     <section
@@ -46,47 +28,10 @@ const TopSellingBranches = ({ className }) => {
           </button>
         </div>
       )}
-      <div className="flex flex-col mt-8 p-5">
-        <h4 className="font-light">Branch List</h4>
-        <p className="text-sm text-[#726C6C]"> Daily Average Sale</p>
-        <div className="flex flex-col mt-2">
-          {data.map((item, index) => (
-            <div
-              key={index}
-              className={`flexBetween ${
-                index !== data.length - 1 && ` border-b `
-              } py-2 border-[#97979780]/50`}
-            >
-              <div className="flex flex-col">
-                <h3 className="text-[#B5B3B3] font-medium">{item.name}</h3>
-                <span className="text-sm font-light text-[#B5B3B3] -translate-y-1">
-                  {item.location}
-                </span>
-              </div>
-              <div>
-                <img
-                  src={
-                    item.grow ? "/icons/growGraph.svg" : "/icons/lossGraph.svg"
-                  }
-                  alt=""
-                />
-              </div>
-              <div className="flexEnd gap-x-2">
-                <span className="font-medium ">{item.value}</span>
-
-                <img
-                  src={item.grow ? "/icons/grow.svg" : "/icons/loss.svg"}
-                  alt=""
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
       {!isFullScreenModalOpen && (
-        <div className="absolute top-4  flexBetween  left-0 pl-5 pr-5 right-0 w-full ">
+        <div className="  flexBetween  pl-5 pr-5 my-3 w-full ">
           <span className="text-[#9F9C9C] font-semibold">
-            Top Selling Departments
+            Top Selling Branches
           </span>
           <div className="flexEnd gap-x-4">
             <button className="flexStart gap-x-2">
@@ -107,6 +52,43 @@ const TopSellingBranches = ({ className }) => {
           </div>
         </div>
       )}
+      <div className="flex flex-col px-5   h-full">
+        <h4 className="font-light">Branch List</h4>
+        <p className="text-sm text-[#726C6C]"> Daily Average Sale</p>
+        <div className="flex flex-col mt-2  h-full overflow-y-auto">
+          {dummy?.map((item, index) => (
+            <div
+              key={index}
+              className={`flexBetween ${
+                index !== dummy?.length - 1 && ` border-b `
+              } py-2 min-h-16 border-[#97979780]/50`}
+            >
+              <div className="flex flex-col">
+                <h3 className="text-[#B5B3B3] font-medium">{item.branch}</h3>
+                <span className="text-sm font-light text-[#B5B3B3] -translate-y-1">
+                  {item.location}
+                </span>
+              </div>
+              <div>
+                <img
+                  src={
+                    item.grow ? "/icons/growGraph.svg" : "/icons/lossGraph.svg"
+                  }
+                  alt=""
+                />
+              </div>
+              <div className="flexEnd gap-x-2">
+                <span className="font-medium ">{item.dailyAverageSale}</span>
+
+                <img
+                  src={item.grow ? "/icons/grow.svg" : "/icons/loss.svg"}
+                  alt=""
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
